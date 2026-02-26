23a62e09ae719031c4c6569d314597327eb006b2
MarcoChenCu <alfredochencu@gmail.com>
2026-02-25 22:26:55 -0600

    Commit inicial :fire::fire:

---
+++ app/Language/es-MX/Login.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    "gcaptcha"                      => "No soy un robot.",
+    "go"                            => "Ingresar",
+    "invalid_gcaptcha"              => "Por favor compruebe que usted no es un robot.",
+    "invalid_installation"          => "La instalación no es correcta, revise el archivo php.ini.",
+    "invalid_username_and_password" => "Usuario y/o contraseña inválidos.",
+    "login"                         => "Iniciar sesión",
+    "logout"                        => "Cerrar sesión",
+    "migration_needed"              => "Una migración de base de datos a {0} empezara después de ingresar.",
+    "password"                      => "Contraseña",
+    "required_username"             => "El nombre de usuario es obligatorio.",
+    "username"                      => "Usuario",
+    "welcome"                       => "Bienvenido a {0}!",
+];
